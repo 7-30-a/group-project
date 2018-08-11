a91663cda9056a8b3dff9f31fafd215dee998bd9
bryanna21 <bryannallcoleman@gmail.com>
2018-08-10 21:12:17 -0400

    added home page and routing

---
+++ src/app/quizzes/quiz6/quiz6.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'quiz6',
+  templateUrl: './quiz6.component.html',
+  styleUrls: ['./quiz6.component.css']
+})
+export class Quiz6Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
