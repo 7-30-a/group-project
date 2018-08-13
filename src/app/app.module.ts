fe3a7f989d06040859f9207190fd1844b89f5998
Unknown <Mahekaru@yahoo.com>
2018-08-13 18:13:29 -0400

    Revert "Merge branch 'feature1'"
    This reverts commit 2a25475fa45be37a5bce072497ee43ebc56444a4.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
-import { TestComponent } from './test/test.component';
 
 
 @NgModule({
@@ -15,7 +14,6 @@ import { TestComponent } from './test/test.component';
     LoginComponent,
     AboutComponent,
     HomeComponent,
-    TestComponent,
   ],
   imports: [
     BrowserModule,
