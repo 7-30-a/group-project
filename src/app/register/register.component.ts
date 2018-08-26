1cf98d44a6e94df1d1f389efd5721c42f670921a
Unknown <Mahekaru@yahoo.com>
2018-08-26 18:13:40 -0400

    Revert "moved routes and modules to server"
    This reverts commit 47a4c14878fb65fef7796ef9fba11659b891b08f.

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,20 +8,12 @@ import { UserService } from '../user.service';
 })
 export class RegisterComponent implements OnInit {
   user: any
-  isloading = false;
 
   register() {
-    console.log("Register!")
     this.userService.register(this.user).subscribe((data: any) => {
       localStorage.setItem('token', data.token) 
     });
   }
-
-  getUser(){
-    console.log("getUser HIT");
-    this.userService.getUser(this.user).subscribe(data => this.user = data,error => console.log(error),() => this.isloading = false);
-  }
-
 
   constructor(private userService: UserService) {
 
